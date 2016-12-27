0b4d007619a46dd6e82e3041833d184dfa469581
xuran <380952691@qq.com>
2016-12-27 22:58:52 +0800

    createMovieCategory

---
+++ VirtualToVPS/createMovieCategory.py
@@ -0,0 +1,13 @@
+import redis
+
+rvirtual = redis.StrictRedis(host='localhost',password='giligilisgay')
+
+keys = rvirtual.smembers('category')
+for key in keys:
+    realkey = key.decode('utf-8')
+    print('realkey = %s' % realkey)
+    movies = rvirtual.smembers('category:%s'%key[1:-1].decode('utf-8'))
+    for movie in movies:
+        realmovie = movie[1:-1].decode('utf-8').strip()
+        print('realmovie = %s'%realmovie)
+        rvirtual.sadd('movieCategory:%s'%realmovie,realkey)
